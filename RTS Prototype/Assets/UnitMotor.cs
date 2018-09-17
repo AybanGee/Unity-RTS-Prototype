254de53dbe9f453ac743dcb74ac0f550ca0e8cd9
Harold <haroldgeronimo3000@gmail.com>
2018-09-18 04:21:16 +0800

    Initial  Commit
    Spawning System

---
+++ RTS Prototype/Assets/UnitMotor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+public class UnitMotor : MonoBehaviour {
+
+	UnityEngine.AI.NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+		agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	public void MoveToPoint(Vector3 point){
+		agent.SetDestination(point);
+	}
+}
